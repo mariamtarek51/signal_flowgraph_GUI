67389a6fe8f2c4e6f570ce704797fc7371980000
MayarElkhouly <101996959+MayarElkhouly@users.noreply.github.com>
2022-04-26 02:28:39 +0200

    Add files via upload

--- src/src/Solver.java
+++ src/src/Solver.java
@@ -1,297 +1,352 @@
-import java.util.*;
+package flow;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Solver {
-    public double[][] segmentsGains;
-    public int numOfNodes;
+	public static double delta;
 
-    public ArrayList<Integer[]> nonTouchingLoops;
-    public ArrayList<Double> nonTouchingLoopGains;
+	private double[][] segmentsGains;
+	private int numOfNodes;
 
+	private ArrayList<Integer[]> nonTouchingLoops;
+	private ArrayList<Double> nonTouchingLoopGains;
 
-    public ArrayList<ArrayList<Integer>> loops;
-    public ArrayList<boolean[]> loopsMask;
-    public ArrayList<Double> loopGains;
-    public ArrayList<ArrayList<Integer>> forwardPaths;
-    public ArrayList<boolean[]> forwardPathsMask;
-    public ArrayList<Double> forwardPathGains;
+	private ArrayList<ArrayList<Integer>> forwardPaths;
+	private ArrayList<boolean[]> forwardPathsMask;
+	private ArrayList<Double> forwardPathGains;
 
-    public void setSFG(double[][] segmentsGains) {
-        numOfNodes = segmentsGains.length;
-        this.segmentsGains = segmentsGains;
-        initialize();
-    }
+	private ArrayList<ArrayList<Integer>> loops;
+	private ArrayList<boolean[]> loopsMask;
+	private ArrayList<Double> loopGains;
 
-    void initialize() {
-        forwardPaths = new ArrayList<ArrayList<Integer>>();
-        forwardPathGains = new ArrayList<Double>();
-        loops = new ArrayList<ArrayList<Integer>>();
-        loopGains = new ArrayList<Double>();
-        nonTouchingLoops = new ArrayList<Integer[]>();
-        nonTouchingLoopGains = new ArrayList<Double>();
-        forwardPathsMask = new ArrayList<boolean[]>();
-        loopsMask = new ArrayList<boolean[]>();
+	public void setSFG(double[][] segmentsGains) {
+		numOfNodes = segmentsGains.length;
+		this.segmentsGains = segmentsGains;
+		initialize();
+	}
 
-        generateFBAndLoops();
-        ArrayList<ArrayList<Integer>> loopLabels = new ArrayList<ArrayList<Integer>>();
-        for (int i = 0; i < loops.size(); i++) {
-            loopLabels.add(new ArrayList<Integer>());
-            loopLabels.get(loopLabels.size() - 1).add(i);
-        }
-        generateNonTouching(loopLabels, 1);
-    }
-    //calculate gains
-    public double calcGain(ArrayList<Integer> arr) {
-        double temp = 1;
-        if (arr.size() > 1) {
-            for (int i = 0; i < arr.size() - 1; i++)
-                temp *= segmentsGains[arr.get(i)][arr.get(i + 1)];
-            return temp;
-        }
-        return segmentsGains[arr.get(0)][arr.get(0)];
+	private void initialize() {
+		forwardPaths = new ArrayList<ArrayList<Integer>>();
+		forwardPathGains = new ArrayList<Double>();
+		loops = new ArrayList<ArrayList<Integer>>();
+		loopGains = new ArrayList<Double>();
+		nonTouchingLoops = new ArrayList<Integer[]>();
+		nonTouchingLoopGains = new ArrayList<Double>();
+		forwardPathsMask = new ArrayList<boolean[]>();
+		loopsMask = new ArrayList<boolean[]>();
 
-    }
+		generateFBAndLoops();
+		ArrayList<ArrayList<Integer>> loopLabels = new ArrayList<ArrayList<Integer>>();
+		for (int i = 0; i < loops.size(); i++) {
+			loopLabels.add(new ArrayList<Integer>());
+			loopLabels.get(loopLabels.size() - 1).add(i);
+		}
+		generateNonTouching(loopLabels, 1);
+	}
+ 
+//calculate gains
+	private double calcGain(ArrayList<Integer> arr) {
+		double temp = 1;
+		if (arr.size() > 1) {
+			for (int i = 0; i < arr.size() - 1; i++)
+				temp *= segmentsGains[arr.get(i)][arr.get(i + 1)];
+			return temp;
+		}
+		return segmentsGains[arr.get(0)][arr.get(0)];
 
-    public void addToLoops(ArrayList<Integer> arr) {
-        arr.add(arr.get(0));   //last node of loop = first node of it
-        if (!isLoopFound(arr)) {
-            loops.add(arr);
-            loopsMask.add(mapNodes(arr));
-            loopGains.add(calcGain(arr));
-        }
-    }
+	}
 
-    public boolean[] mapNodes(ArrayList<Integer> arr) {
-        boolean[] temp = new boolean[numOfNodes];
-        for (int i = 0; i < arr.size(); i++) {
-            temp[arr.get(i)] = true;
-        }
-        return temp;
-    }
+	private void addToLoops(ArrayList<Integer> arr) {
+		arr.add(arr.get(0));   //last node of loop = first node of it
+		if (!isLoopFound(arr)) {
+			loops.add(arr);
+			loopsMask.add(mapNodes(arr));
+			loopGains.add(calcGain(arr));
+		}
+	}
 
-    public boolean isEquivalentLoop(boolean[] arr1, boolean[] arr2) {
-        for (int i = 0; i < arr1.length; i++) {
-            if (arr1[i] != arr2[i])
-                return false;
-        }
-        return true;
-    }
+	private boolean[] mapNodes(ArrayList<Integer> arr) {
+		boolean[] temp = new boolean[numOfNodes];
+		for (int i = 0; i < arr.size(); i++) {
+			temp[arr.get(i)] = true;
+		}
+		return temp;
+	}
+	
+	private boolean isEquivalentLoop(boolean[] arr1, boolean[] arr2) {
+		for (int i = 0; i < arr1.length; i++) {
+			if (arr1[i] != arr2[i])
+				return false;
+		}
+		return true;
+	}
+	
+	private boolean isLoopFound(ArrayList<Integer> arr) {
+		boolean[] loop = mapNodes(arr);
+		for (int i = 0; i < loops.size(); i++) {
+			if (loops.get(i).size() == arr.size()
+					&& isEquivalentLoop(loop, loopsMask.get(i))) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    public boolean isLoopFound(ArrayList<Integer> arr) {
-        boolean[] loop = mapNodes(arr);
-        for (int i = 0; i < loops.size(); i++) {
-            if (loops.get(i).size() == arr.size()
-                    && isEquivalentLoop(loop, loopsMask.get(i))) {
-                return true;
-            }
-        }
-        return false;
-    }
+	private void addToFP(ArrayList<Integer> arr) {
 
-    public void addToFP(ArrayList<Integer> arr) {
+		forwardPaths.add(arr);
+		forwardPathsMask.add(mapNodes(arr));
+		forwardPathGains.add(calcGain(arr));
+	}
 
-        forwardPaths.add(arr);
-        forwardPathsMask.add(mapNodes(arr));
-        forwardPathGains.add(calcGain(arr));
-    }
+	private void generateFBAndLoops() {
+		FBAndLoops(new ArrayList<Integer>(), new boolean[numOfNodes], 0);
+	}
 
-    public void generateFBAndLoops() {
-        FBAndLoops(new ArrayList<Integer>(), new boolean[numOfNodes], 0);
-    }
+	private void FBAndLoops(ArrayList<Integer> path, boolean[] visited, int nodeNum) {
+		path.add(nodeNum);
+		visited[nodeNum] = true;
+		// forward path case
+		if (path.size() > 1 && nodeNum == numOfNodes - 1) {
+			addToFP(new ArrayList<>(path));
+			return;
+		}
+		for (int neighbour = 0; neighbour < numOfNodes; neighbour++) {
+			if (segmentsGains[nodeNum][neighbour] != 0) {
+				if (!visited[neighbour]) {
+					FBAndLoops(path, visited, neighbour);
+					path.remove(path.size() - 1);
+					visited[neighbour] = false;
+					// loop case
+				} else {
+					int index = path.indexOf(neighbour);
+					if (index != -1) {
+						List<Integer> temp = path.subList(index, path.size());
+						addToLoops(new ArrayList<Integer>(temp));
+					}
+				}
+			}
+		}
+	}
 
-    public void FBAndLoops(ArrayList<Integer> path, boolean[] visited, int nodeNum) {
-        path.add(nodeNum);
-        visited[nodeNum] = true;
-        // forward path case
-        if (path.size() > 1 && nodeNum == numOfNodes - 1) {
-            addToFP(new ArrayList<>(path));
-            return;
-        }
-        for (int neighbour = 0; neighbour < numOfNodes; neighbour++) {
-            if (segmentsGains[nodeNum][neighbour] != 0) {
-                if (!visited[neighbour]) {
-                    FBAndLoops(path, visited, neighbour);
-                    path.remove(path.size() - 1);
-                    visited[neighbour] = false;
-                    // loop case
-                } else {
-                    int index = path.indexOf(neighbour);
-                    if (index != -1) {
-                        List<Integer> temp = path.subList(index, path.size());
-                        addToLoops(new ArrayList<Integer>(temp));
-                    }
-                }
-            }
-        }
-    }
+	private void generateNonTouching(ArrayList<ArrayList<Integer>> ArrList, int nth) {
+		Set<List<Integer>> foundbefore = new HashSet<List<Integer>>();
+		boolean moveOnFlag = false;
+		ArrayList<ArrayList<Integer>> nextArrList = new ArrayList<ArrayList<Integer>>();
+		for (int i = 0; i < ArrList.size(); i++) {
+			for (int j = i + 1; j < ArrList.size(); j++) {
+				for (int k = 0; k < ArrList.get(j).size(); k++) {
+					int cand = ArrList.get(j).get(k);
+					ArrayList<Integer> temp = new ArrayList<Integer>();
+					temp.addAll(ArrList.get(i));
+					temp.add(cand);
+					if (isNonTouching(temp)) {
+						Collections.sort(temp);
+						if (!foundbefore.contains(temp)) {
+							foundbefore.add(temp);
+							moveOnFlag = true;
+							nextArrList.add(new ArrayList<Integer>());
+							nextArrList.get(nextArrList.size() - 1)
+									.addAll(temp);
+							nonTouchingLoops.add(temp.toArray(new Integer[temp
+									.size()]));
+							nonTouchingLoopGains.add(getNonTouchingGain(temp));
+						}
+					}
+				}
+			}
 
-    public void generateNonTouching(ArrayList<ArrayList<Integer>> ArrList, int nth) {
-        Set<List<Integer>> foundbefore = new HashSet<List<Integer>>();
-        boolean moveOnFlag = false;
-        ArrayList<ArrayList<Integer>> nextArrList = new ArrayList<ArrayList<Integer>>();
-        for (int i = 0; i < ArrList.size(); i++) {
-            for (int j = i + 1; j < ArrList.size(); j++) {
-                for (int k = 0; k < ArrList.get(j).size(); k++) {
-                    int cand = ArrList.get(j).get(k);
-                    ArrayList<Integer> temp = new ArrayList<Integer>();
-                    temp.addAll(ArrList.get(i));
-                    temp.add(cand);
-                    if (isNonTouching(temp)) {
-                        Collections.sort(temp);
-                        if (!foundbefore.contains(temp)) {
-                            foundbefore.add(temp);
-                            moveOnFlag = true;
-                            nextArrList.add(new ArrayList<Integer>());
-                            nextArrList.get(nextArrList.size() - 1)
-                                    .addAll(temp);
-                            nonTouchingLoops.add(temp.toArray(new Integer[temp
-                                    .size()]));
-                            nonTouchingLoopGains.add(getNonTouchingGain(temp));
-                        }
-                    }
-                }
-            }
+		}
+		if (moveOnFlag) {
+			generateNonTouching(nextArrList, ++nth);
+		}
+	}
 
-        }
-        if (moveOnFlag) {
-            generateNonTouching(nextArrList, ++nth);
-        }
-    }
+	private boolean isNonTouching(ArrayList<Integer> arr) {
+		int flag;
+		// looping over columns
+		for (int i = 0; i < numOfNodes; i++) {
+			flag = 0;
+			// looping over rows
+			for (int j = 0; j < arr.size(); j++) {
+				if (loopsMask.get(arr.get(j))[i])
+					flag++;
+			}
+			if (flag > 1)
+				return false;
+		}
+		return true;
+	}
 
-    public boolean isNonTouching(ArrayList<Integer> arr) {
-        int flag;
-        // looping over columns
-        for (int i = 0; i < numOfNodes; i++) {
-            flag = 0;
-            // looping over rows
-            for (int j = 0; j < arr.size(); j++) {
-                if (loopsMask.get(arr.get(j))[i])
-                    flag++;
-            }
-            if (flag > 1)
-                return false;
-        }
-        return true;
-    }
-    public double getNonTouchingGain(ArrayList<Integer> arr) {
-        double gain = 1;
-        for (int j = 0; j < arr.size(); j++)
-            gain *= loopGains.get(arr.get(j));
-        return gain;
-    }
-    public Double[] getForwardPathGains() {
-        return forwardPathGains.toArray(new Double[forwardPathGains.size()]);
-    }
+	private double getNonTouchingGain(ArrayList<Integer> arr) {
+		double gain = 1;
+		for (int j = 0; j < arr.size(); j++)
+			gain *= loopGains.get(arr.get(j));
+		return gain;
+	}
+	
+	public Double[] getForwardPathGains() {
+		return forwardPathGains.toArray(new Double[forwardPathGains.size()]);  
+	}
 
-    public Double[] getLoopGains() {
-        return loopGains.toArray(new Double[loopGains.size()]);
-    }
+	public Double[] getLoopGains() {
+		return loopGains.toArray(new Double[loopGains.size()]);
+	}
 
-    public Double[] getNonTouchingLoopGains() {
-        return nonTouchingLoopGains.toArray(new Double[nonTouchingLoopGains.size()]);
-    }
+	public Double[] getNonTouchingLoopGains() {
+		return nonTouchingLoopGains.toArray(new Double[nonTouchingLoopGains.size()]);
+	}
+	
+	/*
+	 * convert FPs, loops and nontouching loops into strings 
+	 */
+	public String[] getForwardPaths() {
+		String fbString[] = new String[forwardPaths.size()];
+		int itr = 0;
+		for (ArrayList<Integer> arr : forwardPaths) {  //hena arr de 7t3di 3la elem elem fe forwardPaths
+			fbString[itr] = "";                        //elle no3ha ArrayList
+			for (int i = 0; i < arr.size(); i++) {
+				fbString[itr] += (arr.get(i) + 1) + " ";
+			}
+			itr++;
+		}
+		return fbString;
+	}
+	
+	public String[] getLoops() {
+		String loopsString[] = new String[loops.size()];
+		int itr = 0;
+		for (ArrayList<Integer> arr : loops) {
+			loopsString[itr] = "";
+			for (int i = 0; i < arr.size(); i++) {
+				loopsString[itr] += (arr.get(i) + 1) + " ";
+			}
+			itr++;
+		}
+		return loopsString;
+	}
+	
+	public String[] getNonTouchingLoops() {
+		String[] temp = getLoops();
+		String nonString[] = new String[nonTouchingLoops.size()];
+		int itr = 0;
+		for (Integer[] arr : nonTouchingLoops) {
+			nonString[itr] = "";
 
-    /*
-     * convert FPs, loops and nontouching loops into strings
-     */
-    public String[] getForwardPaths() {
-        String fbString[] = new String[forwardPaths.size()];
-        int itr = 0;
-        for (ArrayList<Integer> arr : forwardPaths) {  //Traverses each element in forward paths
-            fbString[itr] = "";
-            for (int i = 0; i < arr.size(); i++) {
-                fbString[itr] += (arr.get(i) + 1) + " ";
-            }
-            itr++;
-        }
-        return fbString;
-    }
+			if (arr.length > 0)
+				nonString[itr] += temp[arr[0]];
 
-    public String[] getLoops() {
-        String loopsString[] = new String[loops.size()];
-        int itr = 0;
-        for (ArrayList<Integer> arr : loops) {
-            loopsString[itr] = "";
-            for (int i = 0; i < arr.size(); i++) {
-                loopsString[itr] += (arr.get(i) + 1) + " ";
-            }
-            itr++;
-        }
-        return loopsString;
-    }
+			for (int i = 1; i < arr.length; i++)
+				nonString[itr] += " , " + temp[arr[i]];
 
-    public String[] getNonTouchingLoops() {
-        String[] temp = getLoops();
-        String nonString[] = new String[nonTouchingLoops.size()];
-        int itr = 0;
-        for (Integer[] arr : nonTouchingLoops) {
-            nonString[itr] = "";
+			itr++;
+		}
+		return nonString;
+	}
 
-            if (arr.length > 0)
-                nonString[itr] += temp[arr[0]];
+//calculate the transfer function
+	private boolean isNonTouchingWithFP(ArrayList<Integer> arr, int fbNum) {
+		int flag;
+		for (int i = 0; i < numOfNodes; i++) {
+			flag = 0;
+			for (int j = 0; j < arr.size(); j++) {
+				if (loopsMask.get(arr.get(j))[i])
+					flag++;
+			}
+			if (forwardPathsMask.get(fbNum)[i])
+				flag++;
+			if (flag > 1)
+				return false;
+		}
+		return true;
+	}
+	
+	
+	
+	public void deltah(){
+		
+		
+	}
+	
+	public double transferFn() {
+		double current = 0;
+		double delta = 0;
+		double delta1=0;
+		double delta2=0;
+		double delta3=0;
+		int opr = -1;
+		int nth = 1;
+		
+		Double [] tempDou = NeedsData.loopsGain;
+		for (int i = 0; i < tempDou.length; i++) {
+			delta1 += tempDou[i].doubleValue(); 
+		}
+		
+	   tempDou =  NeedsData.nonTouchingloopsGain;
+		for (int i = 0; i < tempDou.length; i++) {
+			delta2 += tempDou[i].doubleValue(); 
+		}
+		
+		/*for (int i = 0; i < nonTouchingLoops.size(); i++) {
+			if (nonTouchingLoops.get(i).length == nth) {
+				current += nonTouchingLoopGains.get(i);
+			} else {
+				delta += opr * current;
+				opr *= -1;
+				++nth;
+			}
 
-            for (int i = 1; i < arr.length; i++)
-                nonString[itr] += " , " + temp[arr[i]];
+		}*/
+		
+		
+	/*	Double [] tempDou = NeedsData.loopsGain;  /////////////////////
+		for (int i = 0; i < tempDou.length; i++) {
+			delta1 += tempDou[i].doubleValue();
+		}*/
+		
+		
+	/*	tempDou = NeedsData.nonTouchingloopsGain;  //////////////////////////////////
+		for (int i = 0; i < tempDou.length; i++) {
+			if(nonTouchingLoops.get(i).length == nth) {
+			delta2 += tempDou[i].doubleValue() ;
+			}else {
+				delta3 += tempDou[i].doubleValue() ;;
+				//opr *= -1;
+				++nth;
+				
+			}
+		}*/
+	
+		
+		
+		delta = 1 - delta1 + delta2 ;      ///////////
+		//delta= 1 -delta;
 
-            itr++;
-        }
-        return nonString;
-    }
-    //calculate the transfer function
-    private boolean isNonTouchingWithFP(ArrayList<Integer> arr, int fbNum) {
-        int flag;
-        for (int i = 0; i < numOfNodes; i++) {
-            flag = 0;
-            for (int j = 0; j < arr.size(); j++) {
-                if (loopsMask.get(arr.get(j))[i])
-                    flag++;
-            }
-            if (forwardPathsMask.get(fbNum)[i])
-                flag++;
-            if (flag > 1)
-                return false;
-        }
-        return true;
-    }
+		double numerator = 0;
+		double deltaN;
 
-    public double transferFn() {
+		for (int i = 0; i < forwardPaths.size(); i++) {
+			deltaN = 1;
+			current = 0;
+			opr = -1;
+			for (int j = 0; j < nonTouchingLoops.size(); j++) {
+				if (isNonTouchingWithFP(
+					new ArrayList<Integer>(Arrays.asList(nonTouchingLoops
+					.get(j))), i)) {
+					current += opr * nonTouchingLoopGains.get(j);
+					opr *= -1;
+				} else
+					break;
+			}
+			deltaN += current;
+			numerator += deltaN * forwardPathGains.get(i);
+		}
 
-        double current = 0;
-        double delta = 0;
-        int opr = -1;
-        int nth = 1;
-        for (int i = 0; i < nonTouchingLoops.size(); i++) {
-            if (nonTouchingLoops.get(i).length == nth) {
-                current += nonTouchingLoopGains.get(i);
-            } else {
-                delta += opr * current;
-                opr *= -1;
-                ++nth;
-            }
-
-        }
-        delta = 1 - delta;
-
-        double numerator = 0;
-        double deltaN;
-
-        for (int i = 0; i < forwardPaths.size(); i++) {
-            deltaN = 1;
-            current = 0;
-            opr = -1;
-            for (int j = 0; j < nonTouchingLoops.size(); j++) {
-                if (isNonTouchingWithFP(
-                        new ArrayList<Integer>(Arrays.asList(nonTouchingLoops
-                                .get(j))), i)) {
-                    current += opr * nonTouchingLoopGains.get(j);
-                    opr *= -1;
-                } else
-                    break;
-            }
-            deltaN += current;
-            numerator += deltaN * forwardPathGains.get(i);
-        }
-
-        return numerator / delta;
-    }
+		return numerator / delta;
+	}
 }
-
